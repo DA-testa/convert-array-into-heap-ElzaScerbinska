c93832cd0fefe7e0724d4e32c30bb25944df8f21
ElzaScerbinska <124616741+ElzaScerbinska@users.noreply.github.com>
2023-03-12 16:55:21 +0000

    -

--- build_heap.py
+++ build_heap.py
@@ -39,13 +39,6 @@ def main():
         n = int(input())
         data = list(map(int, input().split()))
 
-        assert len(data) == n
-        swaps = build_heap(data)
-
-        print(len(swaps))
-        for i, j in swaps:
-            print(i, j)
-
     if "F" in  n:
         filename = input()
         with open(filename, "r") as f:
@@ -53,20 +46,20 @@ def main():
             data = list(map(int, f.readline().split()))
 
     # checks if lenght of data is the same as the said lenght
-        assert len(data) == n
+    assert len(data) == n
 
     # calls function to assess the data 
     # and give back all swaps
-        swaps = build_heap(data)
+    swaps = build_heap(data)
 
     # TODO: output how many swaps were made, 
     # this number should be less than 4n (less than 4*len(data))
 
 
     # output all swaps
-        print(len(swaps))
-        for i, j in swaps:
-            print(i, j)
+    print(len(swaps))
+    for i, j in swaps:
+        print(i, j)
 
 
 if __name__ == "__main__":
